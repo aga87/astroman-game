9052a2097b182e412f5d039a935a3c4fe7615705
aga87 <aga.labonarska@outlook.com>
2023-02-03 18:49:33 +0100

    Initialize a Socket.IO server

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
 import dotenv from 'dotenv';
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 dotenv.config();
 const app: Application = express();
 
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World');
+});
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  /* options */
+});
+
+io.on('connection', socket => {
+  console.log('socket connected', socket.id);
+  socket.on('disconnect', () => {
+    console.log('socket disconnected', socket.id);
+  });
+});
+
 const { PORT } = process.env;
-app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
+httpServer.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
