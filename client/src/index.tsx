8c7cb75bd448efa60084605625e39113443df95d
aga87 <aga.labonarska@outlook.com>
2023-02-05 15:42:24 +0100

    Set up Redux store + type Redux hooks

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 import { App } from './App';
 // import reportWebVitals from './reportWebVitals';
 
@@ -10,7 +12,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
